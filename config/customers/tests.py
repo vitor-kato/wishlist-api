4006cd01db19043e4c1a80c802105f9e2cd7dad5
vitor-kato <vitor.s.kato@gmail.com>
2021-02-11 19:00:16 -0300

    Add initial tests for customer

--- config/customers/tests.py
+++ config/customers/tests.py
@@ -1,1 +1,17 @@
-# Create your tests here.
+import unittest
+
+from django.test import TestCase
+
+from config.base_models import User
+
+from .models import Customer
+
+
+class AuthTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="test-user", password="")
+        Customer.objects.create(user=self.user, name="test-name", email="test-email")
+
+    @unittest.expectedFailure
+    def test_duplicate_email(self):
+        Customer.objects.create(user=self.user, name="test-name", email="test-email")
