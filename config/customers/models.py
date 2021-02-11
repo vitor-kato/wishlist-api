40d2b1c6f416ef6681c8b0635ef069957a1f9a42
vitor-kato <vitor.s.kato@gmail.com>
2021-02-11 18:59:55 -0300

    Add customer model

--- config/customers/models.py
+++ config/customers/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+from config.base_models import BaseAbstractModel, User
+
+
+class Customer(BaseAbstractModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customers")
+    name = models.CharField(max_length=255, blank=False)
+    email = models.EmailField(max_length=255, blank=False, unique=True)
