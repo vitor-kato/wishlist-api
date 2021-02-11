2272730a2eaf147df64bd1e70e3ec47ea3a2b375
vitor-kato <vitor.s.kato@gmail.com>
2021-02-11 18:19:53 -0300

    Update settings to use db from env

--- config/config/settings.py
+++ config/config/settings.py
@@ -103,11 +103,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-    # "default": env.db(),
+    "default": env.db(),
 }
 
 
