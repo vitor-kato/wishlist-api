e100c74d6017fa4be12838ec63f335eed65e6fc4
vitor-kato <vitor.s.kato@gmail.com>
2021-02-11 21:54:19 -0300

    Add wishlist serializer to customer

--- config/customers/serializers.py
+++ config/customers/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
+from wishlist.serializers import WishlistSerializer
 
 from .models import Customer
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     """Serializer for customer data """
+
+    wishlist = WishlistSerializer(read_only=True)
 
     class Meta:
         model = Customer
