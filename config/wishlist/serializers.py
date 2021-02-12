e7e8596a80672b0123c4f7b5387d1011a5622ce9
vitor-kato <vitor.s.kato@gmail.com>
2021-02-12 14:30:18 -0300

    Add internal pagination for products

--- config/wishlist/serializers.py
+++ config/wishlist/serializers.py
@@ -1,4 +1,6 @@
+from django.core.paginator import Paginator
 from rest_framework import serializers
+from rest_framework.settings import api_settings
 
 from .models import Product, Wishlist
 
@@ -11,7 +13,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         # fields = "__all__"
-        exclude = ("id", "external_id", "wishlist", "created_at", "updated_at")
+        exclude = ("id", "wishlist", "created_at", "updated_at")
 
     def get_data(self, instance):
         return instance.get_product_data()
@@ -20,9 +22,29 @@ class ProductSerializer(serializers.ModelSerializer):
 class WishlistSerializer(serializers.ModelSerializer):
     """Serializer for Wishlist data """
 
-    products = ProductSerializer(many=True, read_only=True)
+    total_products = serializers.SerializerMethodField()
+    products = serializers.SerializerMethodField("paginated_products")
 
     class Meta:
         model = Wishlist
         # fields = "__all__"
         exclude = ("created_at", "updated_at")
+
+    def get_total_products(self, instance):
+        return instance.products.count()
+
+    def paginated_products(self, instance):
+        page_size = api_settings.PAGE_SIZE
+        page_number = 1
+        if self.context:
+            page_size = (
+                self.context.get("request").query_params.get("size") or page_size
+            )
+            page_number = (
+                self.context.get("request").query_params.get("page") or page_number
+            )
+
+        paginator = Paginator(instance.products.all(), page_size)
+        products = paginator.page(page_number)
+        serializer = ProductSerializer(products, many=True)
+        return serializer.data
