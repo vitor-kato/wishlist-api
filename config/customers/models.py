dfe920095e5d51f0bfbe27ac4035392670ce281d
vitor-kato <vitor.s.kato@gmail.com>
2021-02-13 14:48:57 -0300

    Add str method to models

--- config/customers/models.py
+++ config/customers/models.py
@@ -7,3 +7,6 @@ class Customer(BaseAbstractModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="customers")
     name = models.CharField(max_length=255, blank=False)
     email = models.EmailField(max_length=255, blank=False, unique=True)
+
+    def __str__(self):
+        return self.email
